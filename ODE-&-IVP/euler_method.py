a0b3d140836b5fa305378972d6d6ab0e6e1b003b
QV-137 <115896668+QuantVortex137@users.noreply.github.com>
2024-07-30 22:26:11 -0600

    Create euler_method.py

---
+++ ODE-&-IVP/euler_method.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ODE, where f(t,y) = y'(t)
+def f(t,y): return t*np.exp(3*t) - 2*y
+
+# Real solution (for plotting if needed)
+def f1(t): return t*np.exp(3*t)/5 - np.exp(3*t)/25 + (1/25*np.exp(2*t))
+
+def euler(a, b, N, y0):
+    """ Euler method in [a,b] interval using
+        N partitions and initial condition y0 """
+    h = (b-a)/N
+    t = np.zeros(N+1)
+    y = np.zeros(N+1)
+    t[0] = a 
+    y[0] = y0
+    for k in range(N):
+        y[k + 1] = y[k] + h*f(t[k], y[k])
+        t[k + 1] = t[k] + h
+    return (t,y)
+
+a = 0 # Superior limit
+b = 1 # Inferior limit
+N = 2 # Partitions --> Related to h=(b-a)/N --> N=(b-a)/h
+y0 = 0 # Initial condition
+(t,y) = euler(a, b, N, y0)
+yex = f1(t)
+
+for i in range(N+1):
+    print("i: {:d}\t t: {:.6f}\t w: {:.6f}\t y(t):\
+          {:.6f}\t |y(t)-w|: {:.6f}\n".format(i, t[i], y[i], yex[i], np.abs(f1(t[i])-y[i])), end="")
+
+# Plotting
+print('Para la malla: ' + str((b-a)/N))
+plt.figure(figsize = (12,8))
+plt.plot(t, y, 'r.-')
+plt.plot(t, yex, 'b')
+plt.xlabel('t')
+plt.ylabel('y')
+plt.legend([ 'Euler', 'Exacta'])
+plt.grid(True)
